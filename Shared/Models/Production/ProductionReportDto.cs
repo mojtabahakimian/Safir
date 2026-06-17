c81df1f476c30a6e9f7e88ac97e6d4712da0f113
mojtabahakimian <72439342+mojtabahakimian@users.noreply.github.com>
2026-06-17 16:49:56 +0330

    اضافه کردن مقدار خروجی پودر

---
+++ Shared/Models/Production/ProductionReportDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Safir.Shared.Models.Production
+{
+    public class ProductionReportDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime? ReportDate { get; set; } = DateTime.Today;
+        public string WheyCompany { get; set; }
+        public TimeSpan? ConcentrationStart { get; set; }
+        public TimeSpan? ConcentrationEnd { get; set; }
+        public decimal? ConcentrationWheyQty { get; set; }
+        public TimeSpan? SprayStart { get; set; }
+        public TimeSpan? SprayEnd { get; set; }
+        public decimal? SprayPowderQty { get; set; }
+        public string CounterNumber { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
